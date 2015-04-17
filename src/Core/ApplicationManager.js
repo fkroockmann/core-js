440d75efa39c6a4fa326e5b82a0689017fdfad07
fkroockmann <florian.kroockmann@lp-digital.fr>
2015-04-17 16:00:02 +0200

    [ApplicationManager] + fix init after load application by service

--- src/Core/ApplicationManager.js
+++ src/Core/ApplicationManager.js
@@ -60,7 +60,6 @@ define('Core/ApplicationManager', ['require', 'BackBone', 'jsclass', 'jquery', '
                 this.state = 0;
                 underscore.extend(this, Backbone.Events);
                 this.config = jQuery.extend(true, this.config, config);
-                this.onInit();
             },
 
             getMainRoute: function () {
@@ -214,14 +213,25 @@ define('Core/ApplicationManager', ['require', 'BackBone', 'jsclass', 'jquery', '
                         if (currentApplication) {
                             currentApplication.onStop();
                         }
+
+                        applicationInfos.init = true;
                         AppContainer.register(applicationInfos);
+
                         applicationInfos.instance.onInit();
                         applicationInfos.instance.onStart();
                         instance = applicationInfos.instance;
                     } else {
                         currentApplication.onStop();
                         /** application already exists call resume */
-                        applicationInfos.instance.onResume();
+
+                        if (applicationInfos.init !== true) {
+                            applicationInfos.instance.onInit();
+                            applicationInfos.instance.onStart();
+                            applicationInfos.init = true;
+                        } else {
+                            applicationInfos.instance.onResume();
+                        }
+
                         instance = applicationInfos.instance;
                     }
                     currentApplication = instance;
